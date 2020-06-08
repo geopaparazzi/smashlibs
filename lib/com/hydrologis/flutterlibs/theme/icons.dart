addab24944a505b2c5b796999973b59f1de19b6f
Andrea Antonello <andrea.antonello@gmail.com>
2020-06-08 10:48:20 +0200

    move marker icon to libs

--- lib/com/hydrologis/flutterlibs/theme/icons.dart
+++ lib/com/hydrologis/flutterlibs/theme/icons.dart
@@ -48,13 +48,13 @@ class SmashIcons {
   static IconData layersIcon = MdiIcons.layers;
   static IconData zoomInIcon = MdiIcons.magnifyPlusOutline;
   static IconData zoomOutIcon = MdiIcons.magnifyMinusOutline;
-  
+
   static IconData importIcon = MdiIcons.import;
   static IconData exportIcon = MdiIcons.export;
 
   static IconData upload = MdiIcons.upload;
   static IconData download = MdiIcons.download;
-  
+
   static IconData locationIcon = MdiIcons.crosshairsGps;
 
   static IconData finishedOk = MdiIcons.checkBold;
@@ -111,8 +111,6 @@ class SmashIcons {
     }
   }
 }
-
-
 
 IconData getSmashIcon(String key) {
   var iconData = MdiIcons.fromString(key);
@@ -268,5 +266,70 @@ class IconsWidgetState extends State<IconsWidget> {
         _visualizeList = []..addAll(_completeList);
       });
     }
+  }
+}
+
+const MARKER_ICON_TEXT_EXTRA_HEIGHT = 30.0;
+const MARKER_ICON_TEXT_EXTRA_WIDTH_FACTOR = 3.0;
+
+/// A map marker icon with an optional label.
+class MarkerIcon extends StatelessWidget {
+  final IconData iconData;
+  final Color iconColor;
+  final double iconSize;
+  final String labelText;
+  final Color labelColor;
+  final Color labelBackColor;
+
+  const MarkerIcon(
+    this.iconData,
+    this.iconColor,
+    this.iconSize,
+    this.labelText,
+    this.labelColor,
+    this.labelBackColor,
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return labelText == null
+        ? Container(
+            child: Icon(
+              iconData,
+              size: iconSize,
+              color: iconColor,
+            ),
+          )
+        : Container(
+            height: iconSize,
+            width: iconSize,
+            child: Column(
+              children: [
+                Container(
+                  child: Icon(
+                    iconData,
+                    size: iconSize,
+                    color: iconColor,
+                  ),
+                ),
+                FittedBox(
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: labelBackColor,
+                      borderRadius: BorderRadius.all(Radius.circular(5)),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.all(3.0),
+                      child: Text(
+                        labelText,
+                        style: TextStyle(
+                            color: labelColor, fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                  ),
+                )
+              ],
+            ),
+          );
   }
 }
