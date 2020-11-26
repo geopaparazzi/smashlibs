70a2b4af71d9519ce2af989316146380786055b0
Andrea Antonello <andrea.antonello@gmail.com>
2020-11-26 11:22:33 +0100

    easy project browsing

--- lib/com/hydrologis/flutterlibs/filesystem/filemanagement.dart
+++ lib/com/hydrologis/flutterlibs/filesystem/filemanagement.dart
@@ -123,6 +123,9 @@ class FileBrowserState extends State<FileBrowser> {
 
   @override
   Widget build(BuildContext context) {
+    bool removePrefix =
+        GpPreferences().getBooleanSync("KEY_FILEBROWSER_DOPREFIX", false);
+
     var upButton = FloatingActionButton(
       heroTag: "FileBrowserUpButton",
       tooltip: "Go back up one folder.",
@@ -144,10 +147,21 @@ class FileBrowserState extends State<FileBrowser> {
         appBar: new AppBar(
           actions: <Widget>[
             IconButton(
+              icon: Icon(removePrefix ? MdiIcons.filter : MdiIcons.filterOff),
+              tooltip: removePrefix ? "View full name" : "Remove prefix",
+              onPressed: () async {
+                await GpPreferences()
+                    .setBoolean("KEY_FILEBROWSER_DOPREFIX", !removePrefix);
+                setState(() {
+                  removePrefix = !removePrefix;
+                });
+              },
+            ),
+            IconButton(
               icon: Icon(onlyFiles
                   ? MdiIcons.folderMultipleOutline
                   : MdiIcons.fileOutline),
-              tooltip: onlyFiles ? "View only Files" : "View everything",
+              tooltip: onlyFiles ? "View everything" : "View only Files",
               onPressed: () {
                 setState(() {
                   onlyFiles = !onlyFiles;
@@ -167,86 +181,125 @@ class FileBrowserState extends State<FileBrowser> {
                   return !isDir;
                 }).toList();
               }
-              return ListView(
-                children: data.map((pathName) {
-                  String parentPath = pathName[0];
-                  String labelParentPath = parentPath;
-                  if (Platform.isIOS) {
-                    labelParentPath = IOS_DOCUMENTSFOLDER +
-                        Workspace.makeRelative(parentPath);
-                  }
-                  String name = pathName[1];
-                  bool isDir = pathName[2];
-                  var fullPath = HU.FileUtilities.joinPaths(parentPath, name);
+              return Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  if (data.isNotEmpty)
+                    FittedBox(
+                      child: Padding(
+                        padding: SmashUI.defaultPadding(),
+                        child: SmashUI.normalText(
+                            Platform.isIOS
+                                ? IOS_DOCUMENTSFOLDER +
+                                    Workspace.makeRelative(data[0][0])
+                                : data[0][0],
+                            color: SmashColors.mainDecorations,
+                            bold: true,
+                            textAlign: TextAlign.left),
+                      ),
+                    ),
+                  Expanded(
+                    child: ListView(
+                      children: data.map((pathName) {
+                        String parentPath = pathName[0];
+                        String name = pathName[1];
+                        String labelName = name;
+                        String addInfo = "";
+                        if (removePrefix) {
+                          if (labelName.startsWith("smash_")) {
+                            labelName = labelName.replaceFirst("smash_", "");
+                            if (labelName.startsWith(RegExp(r'\d{8}_'))) {
+                              addInfo +=
+                                  "${labelName.substring(0, 4)}-${labelName.substring(4, 6)}-${labelName.substring(6, 8)}";
+                              // remove date
+                              labelName =
+                                  labelName.replaceFirst(RegExp(r'\d{8}_'), "");
+                              if (labelName.startsWith(RegExp(r'\d{6}'))) {
+                                // remove time
+                                addInfo +=
+                                    " ${labelName.substring(0, 2)}:${labelName.substring(2, 4)}:${labelName.substring(4, 6)}";
+                                labelName = labelName.replaceFirst(
+                                    RegExp(r'\d{6}_'), "");
+                              }
+                            }
+                          }
+                        }
+                        bool isDir = pathName[2];
+                        var fullPath =
+                            HU.FileUtilities.joinPaths(parentPath, name);
 
-                  IconData iconData = SmashIcons.forPath(fullPath);
-                  Widget trailingWidget;
-                  if (isDir) {
-                    if (widget._doFolderMode) {
-                      // if folder you can enter or select it
-                      trailingWidget = Row(
-                        mainAxisSize: MainAxisSize.min,
-                        children: <Widget>[
-                          IconButton(
+                        IconData iconData = SmashIcons.forPath(fullPath);
+                        Widget trailingWidget;
+                        if (isDir) {
+                          if (widget._doFolderMode) {
+                            // if folder you can enter or select it
+                            trailingWidget = Row(
+                              mainAxisSize: MainAxisSize.min,
+                              children: <Widget>[
+                                IconButton(
+                                  icon: Icon(MdiIcons.checkCircleOutline,
+                                      color: SmashColors.mainDecorations),
+                                  tooltip: "Select folder",
+                                  onPressed: () async {
+                                    await Workspace.setLastUsedFolder(
+                                        parentPath);
+                                    var resultPath = HU.FileUtilities.joinPaths(
+                                        parentPath, name);
+                                    Navigator.pop(context, resultPath);
+                                  },
+                                ),
+                                IconButton(
+                                  icon: Icon(SmashIcons.menuRightArrow),
+                                  tooltip: "Enter folder",
+                                  onPressed: () {
+                                    setState(() {
+                                      currentPath = HU.FileUtilities.joinPaths(
+                                          parentPath, name);
+                                    });
+                                  },
+                                )
+                              ],
+                            );
+                          } else {
+                            trailingWidget = IconButton(
+                              icon: Icon(Icons.arrow_right),
+                              tooltip: "Enter folder",
+                              onPressed: () {
+                                setState(() {
+                                  currentPath = HU.FileUtilities.joinPaths(
+                                      parentPath, name);
+                                });
+                              },
+                            );
+                          }
+                        } else {
+                          // if it gets here, then it is sure no folder mode
+                          trailingWidget = IconButton(
                             icon: Icon(MdiIcons.checkCircleOutline,
                                 color: SmashColors.mainDecorations),
-                            tooltip: "Select folder",
+                            tooltip: "Select file",
                             onPressed: () async {
                               await Workspace.setLastUsedFolder(parentPath);
                               var resultPath =
                                   HU.FileUtilities.joinPaths(parentPath, name);
                               Navigator.pop(context, resultPath);
                             },
-                          ),
-                          IconButton(
-                            icon: Icon(SmashIcons.menuRightArrow),
-                            tooltip: "Enter folder",
-                            onPressed: () {
-                              setState(() {
-                                currentPath = HU.FileUtilities.joinPaths(
-                                    parentPath, name);
-                              });
-                            },
-                          )
-                        ],
-                      );
-                    } else {
-                      trailingWidget = IconButton(
-                        icon: Icon(Icons.arrow_right),
-                        tooltip: "Enter folder",
-                        onPressed: () {
-                          setState(() {
-                            currentPath =
-                                HU.FileUtilities.joinPaths(parentPath, name);
-                          });
-                        },
-                      );
-                    }
-                  } else {
-                    // if it gets here, then it is sure no folder mode
-                    trailingWidget = IconButton(
-                      icon: Icon(MdiIcons.checkCircleOutline,
-                          color: SmashColors.mainDecorations),
-                      tooltip: "Select file",
-                      onPressed: () async {
-                        await Workspace.setLastUsedFolder(parentPath);
-                        var resultPath =
-                            HU.FileUtilities.joinPaths(parentPath, name);
-                        Navigator.pop(context, resultPath);
-                      },
-                    );
-                  }
+                          );
+                        }
 
-                  return ListTile(
-                    leading: Icon(
-                      iconData,
-                      color: SmashColors.mainDecorations,
+                        return ListTile(
+                          leading: Icon(
+                            iconData,
+                            color: SmashColors.mainDecorations,
+                          ),
+                          title: Text(labelName),
+                          subtitle: addInfo.isNotEmpty ? Text(addInfo) : null,
+                          trailing: trailingWidget,
+                        );
+                      }).toList(),
                     ),
-                    title: Text(name),
-                    subtitle: Text(labelParentPath),
-                    trailing: trailingWidget,
-                  );
-                }).toList(),
+                  ),
+                ],
               );
             } else {
               return Center(
