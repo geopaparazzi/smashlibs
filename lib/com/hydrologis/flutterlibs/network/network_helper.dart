6ed6c28363ae8dea2543f8113fc7c7a872ab4875
Andrea Antonello <andrea.antonello@gmail.com>
2021-03-30 15:38:26 +0200

    add allowcert option

--- lib/com/hydrologis/flutterlibs/network/network_helper.dart
+++ lib/com/hydrologis/flutterlibs/network/network_helper.dart
@@ -3,12 +3,17 @@ part of smashlibs;
 /// Class to help out with some common netwrok stuff.
 class NetworkHelper {
   /// Get a new [Dio] instance, if necessary configured.
-  static Dio getNewDioInstance() {
-    bool allowSelfCert = GpPreferences().getBooleanSync(SmashPreferencesKeys.KEY_GSS_SERVER_ALLOW_SELFCERTIFICATE, true);
+  static Dio getNewDioInstance({allowSelfCert}) {
+    if (allowSelfCert == null) {
+      allowSelfCert = GpPreferences().getBooleanSync(
+          SmashPreferencesKeys.KEY_GSS_SERVER_ALLOW_SELFCERTIFICATE, true);
+    }
     Dio dio = Dio();
     if (allowSelfCert) {
-      (dio.httpClientAdapter as DefaultHttpClientAdapter).onHttpClientCreate = (HttpClient client) {
-        client.badCertificateCallback = (X509Certificate cert, String host, int port) => true;
+      (dio.httpClientAdapter as DefaultHttpClientAdapter).onHttpClientCreate =
+          (HttpClient client) {
+        client.badCertificateCallback =
+            (X509Certificate cert, String host, int port) => true;
         return client;
       };
     }
