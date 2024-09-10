bb005a0f562da350a6d1a7cbbbe250335c91e14d
Andrea Antonello <andrea.antonello@gmail.com>
2024-09-10 17:23:37 +0200

    add form url items model

--- lib/com/hydrologis/flutterlibs/forms/models.dart
+++ lib/com/hydrologis/flutterlibs/forms/models.dart
@@ -1,16 +1,33 @@
 part of smashlibs;
 
 class FormHandlerState extends ChangeNotifierPlus {
-
   String? selectedTabName;
+  Map<String, String> formUrlItems = {};
 
   void setSelectedTabName(String name) {
     selectedTabName = name;
     notifyListenersMsg("Formhandler: selected tab changed");
   }
 
-
   void onChanged() {
     notifyListenersMsg("forms changed");
+  }
+}
+
+class FormUrlItemsState extends ChangeNotifierPlus {
+  Map<String, String> formUrlItems = {};
+
+  void setFormUrlItem(String variableName, String value) {
+    setFormUrlItemSilently(variableName, value);
+    notifyListenersMsg("Formhandler: set form url item");
+  }
+
+  void setFormUrlItemSilently(String variableName, String value) {
+    formUrlItems[variableName] = value;
+  }
+
+  void resetFormUrlItems() {
+    formUrlItems = {};
+    notifyListenersMsg("Formhandler: reset form url items");
   }
 }
