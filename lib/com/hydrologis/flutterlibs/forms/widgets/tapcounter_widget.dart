477f2df4b80407fb7854840d6e6b91ceaa00aab6
Samuele Dassatti <samuele@igloo.ooo>
2025-05-07 11:09:28 +0200

    Made tap counter value editable

--- lib/com/hydrologis/flutterlibs/forms/widgets/tapcounter_widget.dart
+++ lib/com/hydrologis/flutterlibs/forms/widgets/tapcounter_widget.dart
@@ -13,7 +13,6 @@ class TapcounterFormWidget extends AFormWidget {
       key: getKey(widgetKey),
       formItem: formItem,
       label: label,
-      isReadOnly: itemReadonly,
     );
   }
 
@@ -36,13 +35,11 @@ class TapcounterFormWidget extends AFormWidget {
 class _TapcounterItemWidget extends StatefulWidget {
   final SmashFormItem formItem;
   final String label;
-  final bool isReadOnly;
 
   _TapcounterItemWidget({
     required Key key,
     required this.formItem,
     required this.label,
-    required this.isReadOnly,
   }) : super(key: key);
 
   @override
@@ -50,19 +47,55 @@ class _TapcounterItemWidget extends StatefulWidget {
 }
 
 class _TapcounterItemState extends State<_TapcounterItemWidget> {
+  late TextEditingController _controller;
+
   @override
-  Widget build(BuildContext context) {
+  void initState() {
+    super.initState();
+    _controller = TextEditingController();
+    _updateControllerTextFromFormItem();
+  }
+
+  @override
+  void didUpdateWidget(_TapcounterItemWidget oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    if (widget.formItem != oldWidget.formItem ||
+        widget.formItem.value != oldWidget.formItem.value) {
+      _updateControllerTextFromFormItem();
+    }
+  }
+
+  int _getFormItemIntValue() {
     int value = 0;
     if (widget.formItem.value != null) {
       if (widget.formItem.value is int) {
-        value = widget.formItem.value;
+        value = widget.formItem.value as int;
       } else if (widget.formItem.value is String) {
-        value = int.tryParse(widget.formItem.value) ?? 0;
+        value = int.tryParse(widget.formItem.value as String) ?? 0;
       } else if (widget.formItem.value is double) {
-        value = widget.formItem.value.toInt();
+        value = (widget.formItem.value as double).toInt();
       }
     }
+    return value;
+  }
 
+  void _updateControllerTextFromFormItem() {
+    final int formItemIntValue = _getFormItemIntValue();
+    if (_controller.text != formItemIntValue.toString()) {
+      _controller.text = formItemIntValue.toString();
+      _controller.selection = TextSelection.fromPosition(
+          TextPosition(offset: _controller.text.length));
+    }
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return InputDecorator(
       decoration: InputDecoration(
         labelText: widget.label,
@@ -85,15 +118,50 @@ class _TapcounterItemState extends State<_TapcounterItemWidget> {
                 Icons.remove_circle_outline,
               ),
               onPressed: () {
+                int valueToDecrement =
+                    int.tryParse(_controller.text) ?? _getFormItemIntValue();
                 setState(() {
-                  int newValue = value - 1;
+                  int newValue = valueToDecrement - 1;
                   widget.formItem.setValue(newValue);
+                  _controller.text = newValue.toString();
+                  _controller.selection = TextSelection.fromPosition(
+                      TextPosition(offset: _controller.text.length));
                 });
               },
             ),
           ),
           Expanded(
-            child: Center(child: SmashUI.normalText(value.toString())),
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 8.0),
+              child: TextFormField(
+                controller: _controller,
+                textAlign: TextAlign.center,
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                  border: InputBorder.none,
+                  contentPadding: EdgeInsets.zero,
+                ),
+                onChanged: (text) {
+                  final int? newIntValue = int.tryParse(text);
+                  if (newIntValue != null) {
+                    if (newIntValue != _getFormItemIntValue()) {
+                      widget.formItem.setValue(newIntValue);
+                    }
+                  }
+                },
+                onEditingComplete: () {
+                  final int? finalValue = int.tryParse(_controller.text);
+                  if (finalValue != null) {
+                    if (finalValue != _getFormItemIntValue()) {
+                      widget.formItem.setValue(finalValue);
+                    }
+                  } else {
+                    _updateControllerTextFromFormItem();
+                  }
+                  FocusScope.of(context).unfocus();
+                },
+              ),
+            ),
           ),
           Expanded(
             child: ElevatedButton(
@@ -105,8 +173,14 @@ class _TapcounterItemState extends State<_TapcounterItemWidget> {
                 Icons.add_circle_outline,
               ),
               onPressed: () {
+                int valueToIncrement =
+                    int.tryParse(_controller.text) ?? _getFormItemIntValue();
                 setState(() {
-                  widget.formItem.setValue(value + 1);
+                  int newValue = valueToIncrement + 1;
+                  widget.formItem.setValue(newValue);
+                  _controller.text = newValue.toString();
+                  _controller.selection = TextSelection.fromPosition(
+                      TextPosition(offset: _controller.text.length));
                 });
               },
             ),
