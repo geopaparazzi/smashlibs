7016cb758b19eedd896449d7295c0727f4b9b0cc
Andrea Antonello <andrea.antonello@gmail.com>
2026-03-31 17:04:38 +0200

    fix gpx elevation colortable mode

--- lib/com/hydrologis/flutterlibs/maps/layers/gpx.dart
+++ lib/com/hydrologis/flutterlibs/maps/layers/gpx.dart
@@ -320,15 +320,19 @@ class GpxSource extends VectorLayerSource implements SldLayerSource {
     if (_tracksRoutesFeatures.isNotEmpty) {
       List<Polyline> lines = [];
 
+      // check if the geometry has elevation, i.e. z is not double.nan
       if (_colorTable.isValid() &&
           _tracksRoutesFeatures.isNotEmpty &&
           _tracksRoutesFeatures[0].geometry is JTS.Geometry &&
+          !(_tracksRoutesFeatures[0].geometry!.getCoordinate()?.z ?? double.nan)
+              .isNaN &&
           minLineElev.isFinite &&
           maxLineElev.isFinite) {
         _tracksRoutesFeatures.forEach((lineFeature) {
           List<LatLng>? list = lineFeature.geometry
               ?.getCoordinates()
-              .map((coord) => LatLng(coord.y, coord.x))
+              .map((coord) =>
+                  LatLngExt(coord.y, coord.x, coord.z, -1, -1, 0, -1))
               .toList();
           if (list != null) {
             EnhancedColorUtility.buildPolylines(lines, list, _colorTable,
